2229f38561f0908f6ad844b71d5da68752843f05
n3k0fi5t <r05922078@ntu.edu.tw>
2017-03-16 11:20:24 +0800

    add practice

---
+++ decorator_practice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def c(a,b):
+  return a+b
+
+def decoratorf(func):
+  def wrapper(f):
+    def wrap(*arg,**karg):
+      res = func(*arg,**karg)
+      res2 = f(res,*arg,**karg)
+      return res2
+    return wrap
+  return wrapper
+
+@decoratorf(c)
+def y(*arg):
+  return reduce(lambda x,y:x+y,arg)
+
+if __name__ == '__main__':
+  print y(1,2)
