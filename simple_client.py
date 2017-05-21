2eaac02e14731379bacc52bfadd487bbf129155d
n3k0fi5t <r05922078@ntu.edu.tw>
2017-05-21 23:08:12 +0800

    update chat client

--- simple_client.py
+++ simple_client.py
@@ -2,7 +2,11 @@
 from __future__ import print_function
 import socket,sys,time
 import threading
-def handleThread(clientsock,cond):
+
+IS_ALIVE = False
+
+def handleThread(clientsock, cond):
+  global IS_ALIVE
   print("\nUsing {} to handle listening\n".format(clientsock.getsockname()))
   while(1):
     # try:
@@ -13,22 +17,27 @@ def handleThread(clientsock,cond):
     # except:
     #   traceback.print_exc()
     #   sys.exit(1)
-    cond.acquire()
     try:
       buf = clientsock.recv(2048)
       if buf :
         if buf != "the send ok signal":
           print("\n{:<50}{},from:{}".format(buf, " "*15, clientsock.getpeername()))
       else:
+        cond.acquire()
+        IS_ALIVE = False
+        cond.notifyAll()
+        print(IS_ALIVE)
+        cond.release()
         print("Detroy thread")
         sys.exit(-1)
+
     except socket.error, e:
       print("Error receiving data %s" %(e))
       sys.exit(1)
-    cond.notifyAll()
-    cond.release()
 
 def main():
+  global IS_ALIVE
+
   port=56122
   host='127.0.0.1'
   cond = threading.Condition()
@@ -40,11 +49,14 @@ def main():
       sys.exit(1)
     try:
       s.connect((host,port))
+      cond.acquire()
+      IS_ALIVE = True
       thread=threading.Thread(target = handleThread,
-                               args = (s,cond), name="")
+                               args = (s, cond), name="")
       #set the thread could be killed at the same time when parent thread was killed
       thread.setDaemon(1)
       thread.start()
+      cond.release()
     except socket.gaierror,e:
       print("Error connect to server %s\n" % e)
       sys.exit(1)
@@ -57,12 +69,20 @@ def main():
           print("close connect to {}".format(s.getpeername()))
           s.close()
           sys.exit(1)
+
+        cond.acquire()
+        is_alive = IS_ALIVE
+        cond.release()
+        if is_alive == False:
+          print("Server offline")
+          sys.exit(-1)
         try:
           s.sendto(cmd, (host,port))
         except (socket.error,socket.gaierror), e:
-          #print("Error sending data %s"% e)
-          print("Server offline")
+          print("Error sending data %s"% e)
+          #print("Server offline")
           sys.exit(-1)
+
 
 if __name__=='__main__':
   main()
