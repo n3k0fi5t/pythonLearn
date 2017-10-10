a5ccb94af99e0551ca79532ac12420f2ad0c0ede
n3k0fi5t <r05922078@ntu.edu.tw>
2017-10-10 18:32:43 +0800

    update gm

--- googlemap_multi-thread.py
+++ googlemap_multi-thread.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from sys import argv
+from sys import argv, exit
+from threading import Thread
+from time import sleep
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,66 +12,70 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup as bs
-from threading import Thread
-from time import sleep
 
 
-SEARCH_STRING =  "角亭"
-WAIT_INTERVAL = 5
+SEARCH_STRING = ""
+WAIT_INTERVAL = 7
 SCROLL_PAUSE_TIME = 0.2
 THREAD_LIMIT = 3
 
 XPATH = {
-    'store' : "//*[@id='pane']/div/div[2]/div/div/div[1]/div[1]/button[1]",
-    'search' : "//*[@id='pane']/div/div[2]/div/div/button/jsl/jsl[7]",
-    'comments' : "//*[@id='pane']/div/div[2]/div/div/div[1]/div[3]/div[2]/div/div[1]/span[3]/ul/li/span",
-    'scroll_down' : "//*[@id='pane']/div/div[2]/div/div/div[2]/div[8]",
-    'store_name' : "//*[@id='pane']/div/div[2]/div/div/div[1]/div[3]/div[1]"
+    'store': "//*[@id='pane']/div/div[2]/div/div/div[1]/div[1]/button[1]",
+    'search': "//*[@id='pane']/div/div[2]/div/div/button/jsl/jsl[7]",
+    'comments': "//*[@id='pane']/div/div[2]/div/div/div[1]/div[3]/div[2]/div/div[1]/span[3]/ul/li/span",
+    'scroll_down': "//*[@id='pane']/div/div[2]/div/div/div[2]/div[8]",
+    'store_name': "//*[@id='pane']/div/div[2]/div/div/div[1]/div[3]/div[1]"
 }
+
 
 def back(driver, xpath):
     ele = driver.find_element_by_xpath(xpath)
     ele.click()
     sleep(WAIT_INTERVAL)
 
+
 def scroll_down(driver):
     # find all comments
     element = driver.find_element_by_xpath(XPATH['scroll_down'])
-    
+
     while(1):
         src = driver.page_source
         for i in range(10):
             element.send_keys(Keys.SPACE)
             sleep(SCROLL_PAUSE_TIME)
-        # sleep(WAIT_INTERVAL)
+
+        # src not update anymore imply down to the last comment
         if len(src) == len(driver.page_source):
-            break    
+            break
+
 
 def get_comments(src):
     # list all comments
     code = bs(src, "html.parser")
     cmts = []
-    for (idx,item) in enumerate(code.findAll(attrs={'class':'section-review-text'})):
+    for (idx, item) in enumerate(code.findAll(attrs={'class': 'section-review-text'})):
         # only save valid comment
-        if len(item.text) > 0: 
+        if len(item.text) > 0:
             cmts.append("{0}".format(item.text))
     return cmts
 
+
 def perform_comment(driver, res, search=True, store_name="", result_name=None):
+    store_name = '  '.join(store_name.split('\n'))
     comment = driver.find_element_by_xpath(XPATH['comments'])
     comment.click()
     sleep(WAIT_INTERVAL)
 
-    print("searching... {}\n".format('  '.join(store_name.split('\n'))))
+    print("searching... {}\n".format(store_name))
     scroll_down(driver)
 
     comment_src = driver.page_source
     # close the driver before parsing the source to save computing resource
     driver.close()
-    print("searching {} finished\n".format('  '.join(store_name.split('\n'))))
+    print("searching {} finished\n".format(store_name))
 
     comments = get_comments(comment_src)
-    res.append({store_name : comments})
+    res.append({store_name: comments})
 
     with open("{0}.txt".format("search_result" if result_name is None else result_name), mode="a") as fp:
         fp.write("###{0}###\n".format(store_name))
@@ -76,7 +83,7 @@ def perform_comment(driver, res, search=True, store_name="", result_name=None):
             c = '\n\t'.join(c.split('\n'))
             fp.write("{0}.\n\t{1}\n".format(idx, c))
         fp.close()
-    
+
     # try:
     #     back(driver, XPATH['store'])
     # except:
@@ -87,7 +94,7 @@ def perform_comment(driver, res, search=True, store_name="", result_name=None):
     #         back(driver, XPATH['search'])
     #     except:
     #         driver.close()
-        
+
 
 def crawl_handler(res, idx, search_string):
 
@@ -95,7 +102,8 @@ def crawl_handler(res, idx, search_string):
     driver.get("https://www.google.com.tw/maps")
 
     # wait for loading
-    ele = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.NAME, "q")))
+    ele = WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.NAME, "q")))
 
     searchbox = driver.find_element_by_name('q')
 
@@ -105,17 +113,21 @@ def crawl_handler(res, idx, search_string):
     """
         this two line of code is equal to ```searchbox.send_keys(Keys.RETURN)```
     """
-    searchbox_button = driver.find_element_by_class_name("searchbox-searchbutton")
+    searchbox_button = driver.find_element_by_class_name(
+        "searchbox-searchbutton")
     searchbox_button.click()
 
     sleep(WAIT_INTERVAL)
 
-    ele = driver.find_elements_by_class_name("section-result-text-content")[idx]
+    ele = driver.find_elements_by_class_name(
+        "section-result-text-content")[idx]
     store_name = ele.text
     ele.click()
     sleep(WAIT_INTERVAL)
 
-    perform_comment(driver, res, store_name=store_name, result_name="{0}_{1}".format(store_name, idx))
+    perform_comment(driver, res, store_name=store_name,
+                    result_name="{0}_{1}".format(store_name, idx))
+
 
 def main(search_string=SEARCH_STRING):
     res = []
@@ -125,7 +137,8 @@ def main(search_string=SEARCH_STRING):
     driver.get("https://www.google.com.tw/maps")
 
     # wait for loading
-    ele = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.NAME, "q")))
+    ele = WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.NAME, "q")))
 
     searchbox = driver.find_element_by_name('q')
 
@@ -135,40 +148,47 @@ def main(search_string=SEARCH_STRING):
     """
         this two line of code is equal to ```searchbox.send_keys(Keys.RETURN)```
     """
-    searchbox_button = driver.find_element_by_class_name("searchbox-searchbutton")
+    searchbox_button = driver.find_element_by_class_name(
+        "searchbox-searchbutton")
     searchbox_button.click()
 
     sleep(WAIT_INTERVAL)
 
+    # get the result tag
     eles = driver.find_elements_by_class_name("section-result-text-content")
-    driver.close()
 
     for i in range(len(eles)):
         res.append([])
 
-        searchthread = Thread(target = crawl_handler,
-                             args = (res[len(res)-1], i, search_string),
-                             name="crawl thread_{0}".format(i))
+        searchthread = Thread(target=crawl_handler,
+                              args=(res[len(res)-1], i, search_string),
+                              name="crawl thread_{0}".format(i))
         searchthread.setDaemon(1)
         threads.append(searchthread)
         searchthread.start()
         print("crawling thread_{0}  start".format(i))
 
-        # limited total threads
+        # limited total crawling threads
         while sum([1 if thread.isAlive() else 0 for thread in threads]) >= THREAD_LIMIT:
             continue
 
-
-    if len(eles) == 0 :
+    # only a result or not found
+    if len(eles) == 0:
         comment = None
         try:
             comment = driver.find_element_by_xpath(XPATH['comments'])
         except:
+            driver.close()
             print("Not find any of {}".format(search_string))
+            print(driver.page_source)
 
+        # store found
         if comment:
-            perform_comment(driver, res, search=False, store_name=search_string, result_name=search_string)
-    else :
+            perform_comment(driver, res, search=False,
+                            store_name=search_string, result_name=search_string)
+    else:
+        driver.close()
+        # wait until all thread finished
         while sum([1 if thread.isAlive() else 0 for thread in threads]) > 0:
             continue
 
@@ -176,6 +196,6 @@ def main(search_string=SEARCH_STRING):
 
 if __name__ == '__main__':
     if len(argv) > 1:
-        main(search_string=argv[1])
+        main(search_string=' '.join(argv[1:]))
     else:
         main()
