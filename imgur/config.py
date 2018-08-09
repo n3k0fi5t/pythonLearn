abc9b87c59bc09d99155dd77ca87eaebb215d364
n3k0fi5t <r05922078@ntu.edu.tw>
2018-08-09 11:30:13 +0800

    modify something

--- imgur/config.py
+++ imgur/config.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ## Imgur key
-client_id = '42e77be42e49943'
-client_secret = '66d7bccfc45977d96a329b5b85cf9494d1ffb63d'
-album_id = 'lxk0xUc'
+client_id = 'Your client id'
+client_secret = 'Your client secret'
+album_id = 'album id'
 
-pa44w0rd = 'zxc7895123'
-acc0un5 = 'rgrg1234'
+pa44w0rd = 'password'
+acc0un5 = 'account'
 
 
